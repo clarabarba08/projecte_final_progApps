c913eb6eee69c516d6ce97c7c5ed6beacf63f2e6
Carles Encinas <carles@ganxo.net>
2023-10-29 12:55:52 +0100

    removed unnecessary import

--- lib/pages/pantalla_edita_recepta.dart
+++ lib/pages/pantalla_edita_recepta.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:tasca_3/classe_recepta.dart';
-import "dart:collection";
 
 class PantallaEditaRecepta extends StatefulWidget {
   static const String route = '/edita_recepta';
