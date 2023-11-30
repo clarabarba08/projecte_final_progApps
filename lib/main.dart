e5b45c766766a3d9cf386cac7925aee329d67616
Clara Barba Armengol <101635232+clarabarba08@users.noreply.github.com>
2023-11-30 22:35:01 +0100

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:tasca_3/pages/pantalla_edita_recepta.dart';
 import 'package:tasca_3/pages/pantalla_llista_receptes.dart';
-import 'package:tasca_3/pages/pantalla_mostra_recepta.dart';
+import 'package:tasca_3/pages/pantalla_grups.dart';
+import 'package:tasca_3/pages/pantalla_usuari.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,16 +13,83 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "App de receptes",
-        theme: ThemeData(
-          primarySwatch: Colors.grey,
-        ),
-        routes: {
+      title: "App de receptes",
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: MyHomePage(),
+      /* routes: {
+        
           PantallaLlistaReceptes.route: (context) =>
               const PantallaLlistaReceptes(),
           PantallaMostraRecepta.route: (context) =>
               const PantallaMostraRecepta(),
           PantallaEditaRecepta.route: (context) => const PantallaEditaRecepta(),
-        });
+        }*/
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePage();
+}
+
+class _MyHomePage extends State<MyHomePage> {
+  var pagActual = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final pags = [
+      const pantallagrups(),
+      const PantallaLlistaReceptes(),
+      const pantallausuari(),
+    ];
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.grey,
+          title: Center(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'CuinaCompartida',
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        ),
+        body: IndexedStack(
+          index: pagActual,
+          children: pags,
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.shifting,
+            currentIndex: pagActual,
+            onTap: (index) {
+              setState(() {
+                pagActual = index;
+              });
+            },
+            items: [
+              BottomNavigationBarItem(
+                icon: const Icon(Icons.group),
+                label: 'Grups',
+                backgroundColor: Colors.grey,
+              ),
+              BottomNavigationBarItem(
+                icon: const Icon(Icons.table_rows),
+                label: 'Receptes',
+                backgroundColor: Colors.grey,
+              ),
+              BottomNavigationBarItem(
+                icon: const Icon(Icons.person),
+                label: 'Usuari',
+                backgroundColor: Colors.grey,
+              ),
+            ]));
   }
 }
